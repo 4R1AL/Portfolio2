a0ae520ce8ff39863608432e209f63502cac5da6
Samrat Nagarkoti <arial64200@gmail.com>
2026-09-05 19:40:55 +0545

    Add movie website

---
+++ assets/scripts/script.js
@@ -0,0 +1,9 @@
+// Sound manager
+const music = document.getElementById("bgMusic");
+
+            document.addEventListener("click", () => {
+                music.play();
+            }, { once: true });
+
+// Copy-right manager
+
